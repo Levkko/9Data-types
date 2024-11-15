c8e21caf1bb5e0ce893a10d5b4c62e7a40ac5545
Your Name <you@example.com>
2024-11-15 16:35:52 +0200

    Update

--- Завдання/3. 2  Довільне ціле число від min до max.js
+++ Завдання/3. 2  Довільне ціле число від min до max.js
@@ -1,2 +1,11 @@
 // Довільне ціле число від min до max 
 
+let min = +prompt("Введіть мінімальне число", 1);
+let max = +prompt("Введіть максимальне число", 5);
+
+function random(min, max) {
+    return min + Math.random() * (max - min);
+  }
+
+alert( random(min, max).toFixed(0) ); 
+
