cdfcce8905badbb3138f57d304ca55e443cdac88
Levko <ganchuklevko@gmail.com>
2024-11-23 14:38:45 +0200

    Update 7. 8  необовязково, але спробуйте.js

--- Завдання/7. 8  необовязково, але спробуйте.js
+++ Завдання/7. 8  необовязково, але спробуйте.js
@@ -2,3 +2,18 @@
 // A function which removes duplication of letters in string. 
 // The function must be case-insensitive.
 
+function removeDuplicationLetters(str) {
+    str = str.toLowerCase();  
+    let result = '';  
+
+    for (let i = 0; i < str.length; i++) {
+        if (!result.includes(str[i])) {  
+            result += str[i]; 
+        }
+    }
+
+    return result;  
+}
+console.log(removeDuplicationLetters('Hello I am Iron Man'));  // 'Helo I am rn '
+
+//Chat GPT. Сам так і не догадався, надто тяжко. Але це ніби не обов"язкове
