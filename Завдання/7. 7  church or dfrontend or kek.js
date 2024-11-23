28ca3b0a09ffd98cd3c5fb2553d9ec775c7353fc
Levko <ganchuklevko@gmail.com>
2024-11-23 14:38:34 +0200

    Update 7. 7  church or dfrontend or kek.js

--- Завдання/7. 7  church or dfrontend or kek.js
+++ Завдання/7. 7  church or dfrontend or kek.js
@@ -2,3 +2,36 @@
 // "church" or "dfrontend" or "kek" otherwise returns false. 
 // The function must be case-insensitive.
 
+function dfrontendCheck(str) { 
+    str = str.toLowerCase();
+    if (str.includes("church")) {
+        return true
+    } else if (str.includes("dfrontend")) {
+        return true
+    } else if (str.includes("kek")) {
+        return true
+    } else {
+        return false
+    }
+ }
+
+dfrontendCheck('Hello DFrontend') // true
+dfrontendCheck('lolkek') // true
+dfrontendCheck('Hello') // false
+
+
+
+
+
+
+
+
+//інший варіант 
+function dfrontendCheck(str) { 
+    str = str.toLowerCase();  
+    return str.includes("church") || str.includes("dfrontend") || str.includes("kek");
+}
+
+console.log(dfrontendCheck('Hello DFrontend'));  // true
+console.log(dfrontendCheck('lolkek'));  // true
+console.log(dfrontendCheck('Hello'));  // false
