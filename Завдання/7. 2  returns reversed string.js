356015c0847de755ff44e384e10a908c5f3024a7
Levko <ganchuklevko@gmail.com>
2024-11-23 14:37:16 +0200

    Update 7. 2  returns reversed string.js

--- Завдання/7. 2  returns reversed string.js
+++ Завдання/7. 2  returns reversed string.js
@@ -1,2 +1,10 @@
 // A function which returns reversed string
 
+function reverse(str) { 
+    let letters = str.split('');
+    letters.reverse();
+    return letters.join('');
+ }
+reverse('abc') // 'cba'
+
+//split,reverse,join - я сам вивчив з інтернету
