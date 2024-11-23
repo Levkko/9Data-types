52ae3e2c410c4c02f8d98d4a20dd8e1875f24f4a
Levko <ganchuklevko@gmail.com>
2024-11-23 14:37:31 +0200

    Update 7. 3  prints the stair picture of size n.js

--- Завдання/7. 3  prints the stair picture of size n.js
+++ Завдання/7. 3  prints the stair picture of size n.js
@@ -1,2 +1,36 @@
 // A function which prints the stair picture of size n
 
+function printStairs(n) { 
+    let str = "";
+    for (let i = 1; i <= n; i++) {
+        str += "#"
+        console.log(str)
+    }
+ }
+printStairs(3);
+
+// #
+// ##
+// ###
+
+
+
+
+
+
+
+
+//Другий варіант
+function printStairs(n) { 
+
+    for (let i = 1; i <= n; i++) {
+        console.log("#".repeat(i));
+    }
+    console.log("#".repeat(n))
+ }
+
+printStairs(3);
+
+// #
+// ##
+// ###
