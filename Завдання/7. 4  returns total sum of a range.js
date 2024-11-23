5a29b8354ee5716c90df8523b12ce47210b39bbd
Levko <ganchuklevko@gmail.com>
2024-11-23 14:37:49 +0200

    Update 7. 4  returns total sum of a range.js

--- Завдання/7. 4  returns total sum of a range.js
+++ Завдання/7. 4  returns total sum of a range.js
@@ -1,2 +1,28 @@
 // A function which returns total sum of a range
 
+function sumRange(start, end) { 
+    if (start > end) {
+        return 0;
+    } else {
+        return start + sumRange(start + 1, end);
+    }
+ }
+sumRange(2, 4) // 9
+sumRange(-1, 3) // 5   
+
+
+
+
+
+
+
+
+
+//інший метод (тяжка математика)
+function sumRange(start, end) {
+    const n = end - start + 1; 
+    return (n * (start + end)) / 2; 
+}
+
+console.log(sumRange(2, 4));  // 9
+console.log(sumRange(-1, 3)); // 5
