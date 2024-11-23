70b215c5a75f27080245869567833a708ad7081f
Levko <ganchuklevko@gmail.com>
2024-11-23 14:38:14 +0200

    Update 7. 6  transforms first and last letter to uppercase.js

--- Завдання/7. 6  transforms first and last letter to uppercase.js
+++ Завдання/7. 6  transforms first and last letter to uppercase.js
@@ -1,3 +1,25 @@
 // A function which transforms first and last letter 
 // to uppercase (use built in string's method).
 
+function firstAndLastToUpper(str) { 
+    let result = str[0].toUpperCase() + str.slice(1); 
+    return result.slice(0, -1) + result[result.length - 1].toUpperCase();
+ }
+ 
+firstAndLastToUpper('abc') // 'AbC'
+
+
+
+
+
+
+
+
+
+//інший метод (тяжкіше додуматись, але код кращий)
+function firstAndLastToUpper(str) { 
+    return str[0].toUpperCase() + str.slice(1, -1) + str[str.length - 1].toUpperCase();
+}
+
+console.log(firstAndLastToUpper('abc'));  // 'AbC'
+
