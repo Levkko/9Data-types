d7a31dc30a24ab1c702540b6b3c956ad59348ca0
Levko <ganchuklevko@gmail.com>
2024-11-23 14:38:02 +0200

    Update 7. 5  returns the smallest of three numbers.js

--- Завдання/7. 5  returns the smallest of three numbers.js
+++ Завдання/7. 5  returns the smallest of three numbers.js
@@ -1,2 +1,28 @@
 // Write a function which returns the smallest of three numbers
 
+function min(a, b, c) { 
+    if (a <= b && a <= c) {
+        return  a;
+    } else if (b <= a && b <= c) {
+        return b;
+    } else if (c <= a && c <= b) {
+        return c;
+    }
+ }
+min(10, 5, 11) // 5
+min(3, 8, 4) // 3
+
+
+
+
+
+
+
+
+//інший, набагато легший варіант (згадав про нього аж після if else)
+function min(a, b, c) {
+    return Math.min(a, b, c);
+}
+
+console.log(min(10, 5, 11));  // 5
+console.log(min(3, 8, 4));    // 3
